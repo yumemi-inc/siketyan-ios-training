b6d72dd0d2a9b0ea66ea9628ee147516d4581f6c
Natsuki Ikeguchi <me@s6n.jp>
2023-11-05 02:03:34 +0900

    feat: Show the random weather condition with images

--- TrainingApp/ViewController.swift
+++ TrainingApp/ViewController.swift
@@ -5,15 +5,35 @@
 //  Created by Naoki Ikeguchi on 2023/11/04.
 //
 
+import OSLog
 import UIKit
+import YumemiWeather
 
 class ViewController: UIViewController {
+    @IBOutlet private var weatherImageView: UIImageView!
 
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view.
     }
 
+    @IBAction func onReloadButtonClick() {
+        let weather = YumemiWeather.fetchWeatherCondition()
 
+        Logger().info("\(weather)")
+
+        switch weather {
+        case "sunny":
+            weatherImageView.image = #imageLiteral(resourceName: "Sunny")
+            weatherImageView.tintColor = UIColor.red
+        case "cloudy":
+            weatherImageView.image = #imageLiteral(resourceName: "Cloudy")
+            weatherImageView.tintColor = UIColor.gray
+        case "rainy":
+            weatherImageView.image = #imageLiteral(resourceName: "Rainy")
+            weatherImageView.tintColor = UIColor.blue
+        default:
+            Logger().error("Unknown weather condition.")
+        }
+    }
 }
-
